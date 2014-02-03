22fed649127db0d5b24dbf2f4650f119a0b94cb0
Tang Lu <tanglu2004@gmail.com>
2014-02-03 20:06:03 +0900

    Update main.cpp

--- smart-pointer/main.cpp
+++ smart-pointer/main.cpp
@@ -2,10 +2,10 @@
 
 int main() {
 	SmartPointer<char> cp1(new char('a'));
-  SmartPointer<char> cp2(cp1);
-  SmartPointer<char> cp3;
-  cp3 = cp2;
-  cp3 = cp1;
+	SmartPointer<char> cp2(cp1);
+	SmartPointer<char> cp3;
+	cp3 = cp2;
+	cp3 = cp1;
 	cp3 = cp3;
 	SmartPointer<char> cp4(new char('b'));
 	cp3 = cp4;
